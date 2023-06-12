09ec9703f1e4f08062d1e464dedd5fbfcbc64fc9
thgs <theogpl57@gmail.com>
2023-06-12 19:55:52 +0100

    Expand terminal attributes example

--- examples/terminal-attributes.php
+++ examples/terminal-attributes.php
@@ -26,33 +26,74 @@ $xterm->subscribe(
     }
 );
 
-
 $xterm
     // boot
     ->setPrivateModeSaveCursorAndEnterAlternateScreenBuffer()
     ->hideCursor()
-    // set colors
-    ->bgBlue()
-    ->brightYellow()
-    ->eraseAll()
-    // Primary DA
-    ->moveCursorTo(2, 2)
-    ->write("Terminal ID ..... : $primary->terminalId")
-    ->moveCursorTo(3, 2)
-    ->write("Capabilities .... : " . implode(' ', $primary->capabilities))
-    // Secondary DA
-    ->moveCursorTo(6, 2)
-    ->write("Type ............ : $secondary->type")
-    ->moveCursorTo(8, 2)
-    ->write("Version ......... : $secondary->version")
-    ->moveCursorTo(10, 2)
-    ->write("Cartridge Reg. .. : $secondary->cartridgeRegistration")
-    //
-    ->moveCursorTo(15, 10)
-    ->blink()
-    ->write("Press any key to exit")
-    ->normal()
     ->flush()
 ;
+
+EventLoop::repeat(1, function () use ($xterm, $primary, $secondary) {
+    $xterm
+        // set colors
+        ->bgBlue()
+        ->brightYellow()
+        ->eraseAll();
+
+    $view = [
+        // Primary
+        'Terminal ID' => $primary->terminalId,
+        'Capabilities' => "\n\t" . implode(
+            " ",
+            // only first letter for when its running on xterm
+            array_map(fn ($capability) => substr($capability, 0, 1), $primary->capabilities)
+        ),
+
+        // Secondary
+        'Type' => $secondary->type,
+        'Version' => $secondary->version,
+        'Cartridge Reg' => $secondary->cartridgeRegistration,
+
+        // Sizes
+        'TextArea Position' => implode(' x ', $xterm->reportTextAreaPosition()),
+        'TextArea Size (px)' => implode(' x ', $xterm->reportTextAreaSizePixels()),
+        'TextArea Size (char)' => implode(' x ', $xterm->reportTextAreaSizeCharacters()),
+        'Window Size (px)' => implode(' x ', $xterm->reportWindowSizePixels()),
+        'Screen Size (px)' => implode(' x ', $xterm->reportScreenSizePixels()),
+        'Screen Size (char)' => implode(' x ', $xterm->reportScreenSizeCharacters()),
+        'Character size (px)' => implode(' x ', $xterm->reportCharacterSizePixels()),
+    ];
+
+    $lines = [
+        [2,2],
+        [3,2],
+        [6,2],
+        [8,2],
+        [10,2],
+        [2, 40],
+        [3, 40],
+        [4, 40],
+        [6, 40],
+        [8, 40],
+        [9, 40],
+        [11, 40],
+    ];
+
+    $i = 0;
+    foreach ($view as $title => $data) {
+        $element = str_pad($title . ' ', 25, '.') . ' : ' . $data;
+        $xterm
+            ->moveCursorTo(...$lines[$i++])
+            ->write($element);
+    }
+        //
+    $xterm
+        ->moveCursorTo(15, 10)
+        ->blink()
+        ->write("Press any key to exit or resize/move your screen")
+        ->normal()
+        ->flush()
+    ;
+});
 
 EventLoop::run();
